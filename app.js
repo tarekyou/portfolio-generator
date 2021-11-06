c5276f4157a0a4522534cf479cba7f14b0028b50
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-11-06 14:40:11 -0700

    differentiated between featured and non featured projects

--- app.js
+++ app.js
@@ -16,8 +16,8 @@
 
 
 const inquirer = require ('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err =>{
@@ -167,5 +167,10 @@ if (!portfolioData.projects) {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('./index.html', pageHTML, err =>{
+    if(err) throw err;
+    console.log("Portfolio complete! check out index.html to see the output!")
+});
   });
