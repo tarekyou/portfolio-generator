f56a3c8229e888a1a853eeb29e37cc7b900ae6ed
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-11-07 21:52:41 -0800

    finished-site

--- app.js
+++ app.js
@@ -16,7 +16,8 @@
 
 
 const inquirer = require ('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
+const {writeFile, copyFile} = require('./util/generate-site.js')
 const generatePage = require('./src/page-template.js');
 // const pageHTML = generatePage(name, github);
 
@@ -164,13 +165,47 @@ if (!portfolioData.projects) {
 //                     return portfolioData;
 //                 }
 //               });
+
+
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-      const pageHTML = generatePage(portfolioData);
-
-fs.writeFile('./index.html', pageHTML, err =>{
-    if(err) throw err;
-    console.log("Portfolio complete! check out index.html to see the output!")
-});
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
+
+
+
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//       const pageHTML = generatePage(portfolioData);
+
+// fs.writeFile('./dist/index.html', pageHTML, err =>{
+//     if(err) {
+//       console.log(err);
+//       return
+//     };
+//     console.log("Portfolio complete! check out index.html to see the output!");
+//     fs.copyFile('./src/style.css', './dist/style.css', err => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Style sheet copied successfully!');
+//     });
+// });
+//   });
